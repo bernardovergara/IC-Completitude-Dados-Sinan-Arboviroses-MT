73f8fca61e01801b8f6b22baff598e22fd544a30
bernasveroli <bernardovergara789@gmail.com>
2026-02-21 23:14:59 -0400

    Atualização do gitignore e nolint no incompletude_calc

--- source/incompletude_calc.r
+++ source/incompletude_calc.r
@@ -10,29 +10,29 @@ calcular_incompletude <- function(dados, filtro_dict) {
 
   # IDADE (obrigatória)
   nu_idade_n_incomp <- dados |>
-    dplyr::filter(is.na(NU_IDADE_N))
+    dplyr::filter(is.na(NU_IDADE_N)) # nolint
 
   # SEXO (obrigatória)
   cs_sexo_incomp <- dados |>
-    dplyr::filter(is.na(CS_SEXO) | CS_SEXO == "Ignorado")
+    dplyr::filter(is.na(CS_SEXO) | CS_SEXO == "Ignorado") # nolint
 
   # GESTANT (obrigatória)
   cs_gestant_incomp <- dados |>
-    dplyr::filter(CS_SEXO == "Feminino") |>
+    dplyr::filter(CS_SEXO == "Feminino") |> # nolint
     dplyr::filter(
-      is.na(CS_GESTANT) |
+      is.na(CS_GESTANT) | # nolint
         CS_GESTANT == "Não se aplica" |
         CS_GESTANT == "Ignorado"
     )
 
   # RAÇA (essencial)
   cs_raca_incomp <- dados |>
-    dplyr::filter(is.na(CS_RACA) | CS_RACA == "Ignorado")
+    dplyr::filter(is.na(CS_RACA) | CS_RACA == "Ignorado") # nolint
 
   # ESCOLARIDADE (essencial)
   cs_escol_n_incomp <- dados |>
     dplyr::filter(
-      is.na(CS_ESCOL_N) |
+      is.na(CS_ESCOL_N) | # nolint
         CS_ESCOL_N == "Não se aplica" |
         CS_ESCOL_N == "09"
     )
